84c208d47e4f4bd0a70e100f276b659b5043725b
Branko <branko.stanisic02@gmail.com>
2023-11-02 19:42:08 -0400

    Complete read.me

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Project-1-Game
+
+Welcome to TRIVIAGRAPHY🌍 - the game where you can put your geography skills to the test!
+ - The rules of the games are simple! Just pick the answer that best suits the question.
+ - You will have 10 seconds to answer each question.
+ - Once answered, you will also have 3 seconds to review the question.
+ - If you get MORE than 15 out of 20 questions correct, YOU WIN!🏆 Less than that will result in a LOST!❌
+ - Let's play some TRIVIAGRAPHY🌍
--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const questionContainer = document.getElementById('divId');
 const questionEl = document.getElementById('question-bar');
 const answerEl = document.querySelectorAll('.answer');
 const resetGame = document.getElementById('reset-button');
+const scoreEl = document.getElementById('spanScore')
+const gameOver = document.getElementById('score-id')
 
 let counter = 0;
 let timer;
@@ -24,9 +26,15 @@ function setNextQuestion() {
     })
     showQuestion();
     startTimer();
+    addClick();
     } else {
         questionContainer.classList.add('hide');
         resetGame.classList.remove('hide');
+        if (score > 15) {
+        gameOver.innerText = '🏆You win!✅'     
+        } else {
+        gameOver.innerText = '😓You lose!❌'
+        }
     }
 }
 
@@ -45,12 +53,38 @@ function startTimer() {
     }, 10000);
 }
 
-answerEl.forEach(el => {
-    el.addEventListener('click', (e) => {
-        clearTimeout(timer);
-        selectAnswer(e);
-});
-})
+function addClick() {answerEl.forEach(el => {
+    el.addEventListener('click', buttonEvents
+);
+})}
+
+addClick();
+
+function limitClick() {answerEl.forEach(el => {
+    el.removeEventListener('click', buttonEvents
+);
+})}
+
+function buttonEvents(e) {
+    clearTimeout(timer);
+    selectAnswer(e);
+}
+
+function selectAnswer(e) {
+    limitClick();
+    let userAnswer = e.target.id;
+    let button = e.target;
+    if (userAnswer === questions[counter].correctAnswer) {
+        button.style.backgroundColor = 'green';
+        score++;
+        scoreEl.innerText = score;
+        counter++;
+    } else {
+        button.style.backgroundColor = 'red';
+        counter++;
+    }
+    setTimeout(() => setNextQuestion(), 3000);
+}
 
 function reset() {
     location.reload();
@@ -107,20 +141,154 @@ const questions = [
     },
     correctAnswer: 'b'
 },
+    {
+        question: 'What is the capital of Turkey?',
+    answers: {
+        a: 'Izmir',
+        b: 'Istanbul',
+        c: 'Ankara',
+        d: 'Bursa'
+    },
+    correctAnswer: 'c'
+},
+    {
+        question: 'What is the smallest country?',
+    answers: {
+        a: 'Vatican',
+        b: 'San Marino',
+        c: 'Monaco',
+        d: 'Nauru'
+    },
+    correctAnswer: 'a'
+},
+    {
+        question: 'What is the largest ocean?',
+    answers: {
+        a: 'Pacific',
+        b: 'Indian',
+        c: 'Atlantic',
+        d: 'Arctic'
+    },
+    correctAnswer: 'a'
+},
+    {
+        question: 'What is the largest desert?',
+    answers: {
+        a: 'Sahara',
+        b: 'Arctic',
+        c: 'Antartica',
+        d: 'Gobi'
+    },
+    correctAnswer: 'c'
+},
+    {
+        question: 'What is the largest continent?',
+    answers: {
+        a: 'Africa',
+        b: 'Asia',
+        c: 'Europe',
+        d: 'Australia'
+    },
+    correctAnswer: 'b'
+},
+    {
+        question: 'What is the highest mountain?',
+    answers: {
+        a: 'Mt. Everest',
+        b: 'Mt. K2',
+        c: 'Mt. Lhotse',
+        d: 'Mt. Makalu'
+    },
+    correctAnswer: 'a'
+},
+    {
+        question: 'Where is “The Great Wall” located?',
+    answers: {
+        a: 'Brazil',
+        b: 'Japan',
+        c: 'China',
+        d: 'Germany'
+    },
+    correctAnswer: 'c'
+},
+    {
+        question: 'Which country borders Mexico?',
+    answers: {
+        a: 'Brazil',
+        b: 'Panama',
+        c: 'Argentina',
+        d: 'USA'
+    },
+    correctAnswer: 'd'
+},
+    {
+        question: 'What is the correct spelling of this country?',
+    answers: {
+        a: 'Kyrgystan',
+        b: 'Kyrgyzstan',
+        c: 'Kirgystan',
+        d: 'Kyrgysztan'
+    },
+    correctAnswer: 'b'
+},
+    {
+        question: 'What is the largest state in USA?',
+    answers: {
+        a: 'California',
+        b: 'Texas',
+        c: 'Montana',
+        d: 'Alaska'
+    },
+    correctAnswer: 'd'
+},
+    {
+        question: 'What is the capital of Canada?',
+    answers: {
+        a: 'Toronto',
+        b: 'Vancouver',
+        c: 'Ottawa',
+        d: 'Montreal'
+    },
+    correctAnswer: 'c'
+},
+    {
+        question: 'How many days is 1 year?',
+    answers: {
+        a: '365',
+        b: '366',
+        c: '360',
+        d: '365.25'
+    },
+    correctAnswer: 'd'
+},
+    {
+        question: 'How many official languages are spoken in India?',
+    answers: {
+        a: '22',
+        b: '1',
+        c: '2',
+        d: '47'
+    },
+    correctAnswer: 'a'
+},
+    {
+        question: 'What country owns Greenland?',
+    answers: {
+        a: 'Russia',
+        b: 'USA',
+        c: 'Norway',
+        d: 'Denmark'
+    },
+    correctAnswer: 'd'
+},
+    {
+        question: 'How many countries have non-rectangular flag shape?',
+    answers: {
+        a: 'None',
+        b: '1',
+        c: '2',
+        d: '3'
+    },
+    correctAnswer: 'd'
+},
 ]
-score = '#spanScore';
-
-function selectAnswer(e) {
-    let userAnswer = e.target.id;
-    let button = e.target
-    if (userAnswer === questions[counter].correctAnswer) {
-        button.style.backgroundColor = 'green'
-        score++;
-    } else {
-        button.style.backgroundColor = 'red'
-    }
-
-
-    counter++;
-    setTimeout(() => setNextQuestion(), 3500)
-}
